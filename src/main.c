e3e3bf03e74898f6097c5e9f6bdac11d7f0e1a22
karsh <utkarsh@cyberfear.com>
2025-04-24 18:11:46 +1000

    chore: main logic
    main reads for input values and ignore them until "MS" key is detected.
    it then records values till '}' char. it then looks for "ES" key and
    records that till ']'.
    
    i reckon i can simplify this logic given in my json convention the
    number of chars between MS ending '}' and ES starting '[' is fixed.

--- src/main.c
+++ src/main.c
@@ -15,11 +15,43 @@ int main( void )
 
     while ( TRUE )
     {
+        Property *lCurrentSelected = NULL;
+        int lDataIndex = 0;
         char lSingleChar;
 
         LinkFetch( &lSingleChar );
         if ( '"' == lSingleChar )
         {
+            LinkFetch( &lSingleChar );
+
+            if ( 'M' == lSingleChar )
+            {
+                lCurrentSelected = MS;
+                lDataIndex = 0;
+            }
+            else if ( 'E' == lSingleChar && 0 != lDataIndex )
+            {
+                lCurrentSelected = ES;
+                lDataIndex = 0;
+            }
+            else
+            {
+                continue;
+            }
+
+            while ( lSingleChar != lCurrentSelected->BracketStart ) LinkFetch( &lSingleChar );
+        }
+
+        if ( lCurrentSelected )
+        {
+            while ( lSingleChar != lCurrentSelected->BracketEnd )
+            {
+                LinkFetch( &lSingleChar );
+                lCurrentSelected->Data[lDataIndex++] = lSingleChar;
+            }
+
+            lCurrentSelected->Data[lDataIndex-1] = '\0';
+            lCurrentSelected->Process( lCurrentSelected->Data, &lCurrentSelected->Output );
         }
 
         if ( ES->Output && MS->Output )

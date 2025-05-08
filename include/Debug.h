f4c401e085bdc427f27a569dc104d10a49657718
karsh <utkarsh@cyberfear.com>
2025-05-08 16:01:49 +1000

    style: project path debug
    - removed the whole project path from debug logs, redundant

--- include/Debug.h
+++ include/Debug.h
@@ -38,21 +38,27 @@
         TERM_BLUE, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
 
 #else
-    #define debug( M, ... ) fprintf( stderr, "%s[DEBUG]%s (%s:%d)%s " M "\n%s",\
-        TERM_BLUE, TERM_GREEN, __FILE__, __LINE__, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
+    #define PROJECT_ROOT "Users/zul/Projects/SWE30011/Assignment2/"
+
+    #define RELATIVE_PATH(file) (strstr(file, PROJECT_ROOT) ?\
+            strstr(file, PROJECT_ROOT) + strlen(PROJECT_ROOT) : file)
+
+    #define debug( M, ... ) fprintf( stderr, "%s[DEBUG]%s (%s:%d)%s " M "\n%s", TERM_BLUE,\
+        TERM_GREEN, RELATIVE_PATH(__FILE__), __LINE__, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
 
     #define clean_errno() ( errno == 0 ? "None" : strerror(errno) )
 
-    #define log_error( M, ... ) fprintf( stderr,\
-        "%s[ERROR]%s (%s:%d: errno: %s)%s " M "\n%s", TERM_RED, TERM_GREEN, __FILE__, __LINE__,\
-        clean_errno(), TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
+    #define log_error( M, ... ) fprintf( stderr, "%s[ERROR]%s (%s:%d: errno: %s)%s " M "\n%s",\
+        TERM_RED, TERM_GREEN, RELATIVE_PATH(__FILE__), __LINE__, clean_errno(),\
+        TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
 
-    #define log_warn( M, ... ) fprintf(stderr,\
-        "%s[WARN]%s (%s:%d: errno: %s)%s " M "\n%s", TERM_YELLOW, TERM_GREEN, __FILE__, __LINE__,\
-        clean_errno(), TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
+    #define log_warn( M, ... ) fprintf(stderr, "%s[WARN]%s (%s:%d: errno: %s)%s " M "\n%s",\
+        TERM_YELLOW, TERM_GREEN, RELATIVE_PATH(__FILE__), __LINE__, clean_errno(),\
+        TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
 
     #define log_info( M, ... ) fprintf( stderr, "%s[INFO]%s (%s:%d)%s " M "\n%s",\
-        TERM_BLUE, TERM_GREEN, __FILE__, __LINE__, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
+        TERM_BLUE, TERM_GREEN, RELATIVE_PATH(__FILE__), __LINE__,\
+        TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
 
 #endif
 
